5b971e297d60b7d9d749060a3e9249726ff792ca
Junior Martins <amjr.box@gmail.com>
2022-03-19 18:23:09 -0300

    First commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,36 @@
+import { LoaderFunction, useLoaderData } from 'remix'
+import { supabase } from '~/utils/supabase-client.server'
+
+type PostsModel = {
+  post_id: number
+  post_uuid: string
+  post_author: string
+  post_title: string
+  post_text: string
+  post_situation: string
+  post_created_at: string
+}
+
+export const loader: LoaderFunction = async () => {
+  const { data: posts } = await supabase.from<PostsModel>('posts').select('*')
+
+  return posts
+}
+
+export default function Index() {
+  const posts = useLoaderData<PostsModel[]>()
+
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Blog Remix com Supabase</h1>
+      <ul>
+        {posts?.map(post => (
+          <li key={post.post_uuid}>
+            <p>{post.post_title}</p>
+            <p>{post.post_text}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
